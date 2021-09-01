c59411d9129c49fe5f4400f2795ae80af3a2f484
ADHIKARI KRISHNA <45994360+krishdb38@users.noreply.github.com>
2021-09-01 22:08:20 +0900

    ..

--- mozila_django_tutorial/locallibrary/catalog/models.py
+++ mozila_django_tutorial/locallibrary/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 
 class Genre(models.Model):
@@ -42,3 +43,15 @@ class Book(models.Model):
         return reverse("book_detail", args=[self(self.id)])
 
     # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
+
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library )
+
+    Args:
+        models ([type]): [description]
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book ")
+    book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
