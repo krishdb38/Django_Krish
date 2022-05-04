e4f8d410fd066f55e602f83449f03c851cf41111
ADHIKARI KRISHNA <45994360+krishdb38@users.noreply.github.com>
2022-05-04 09:48:08 +0900

    ..

--- books/CMS_project/CMS_app/StaffViews.py
+++ books/CMS_project/CMS_app/StaffViews.py
@@ -161,3 +161,41 @@ def staff_feedback_save(request):
             list_data.append(data_small)
         return redirect(JsonResponse(json.dumps(list_data), content_type = 'application/json', safe=False))
         
+    @csrf_exempt
+    def save_attendance_data(request):
+        # Get Values From Staff Take Attendance form via AJAX (JS)
+        # Use get list to access HTML array/ List Input data
+        
+        student_ids = request.POST.get('students_ids')
+        subject_id = request.POST.get('subject_id')
+        attendance_date = request.POST.get('attendance_date')
+        session_year_id = request.POST.get('session_year_id')
+        
+        subject_model = Subjects.objects.get(id=subject_id)
+        session_year_model = SessionYearModel.objects.get(id = session_year_id)
+        
+        json_student = json.loads(student_ids)
+        
+        try:
+            # First Attendance Data is Saved on Attendance 
+            attendance = Attendance(subject_id = subject_model, attendance_date = attendance_date, session_year_id = session_year_model)
+            attendance.save()
+            
+            for stud in json_student:
+                # Attendance of Individual Student Saved on AttendanceReport Model
+                student = Students.objects.get(admin = stud['id'])
+                attendance_report = AttendanceReport(student_id = student,
+                                                     attendance_id = attendance,
+                                                     status = stud['status'])
+                attendance_report.save()
+            return HttpResponse('OK')
+        except :
+            return HttpResponse("Error")
+        
+def staff_update_attendance(request):
+    subjects = Subjects.objects.filter(staff_id = request.user.id)
+    session_years = SessionYearModel.objects.all()
+    context = {
+        "subjects":subjects, 
+        "session_years" : session_years
+    }
